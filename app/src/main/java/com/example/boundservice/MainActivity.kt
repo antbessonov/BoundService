141ec45c8f20c35a5e58a246540a9775245c7423
antbessonov <anton@bessonov.org>
2022-08-05 20:49:10 +0700

    Подключение к BoundService.kt в MainActivity.kt

--- app/src/main/java/com/example/boundservice/MainActivity.kt
+++ app/src/main/java/com/example/boundservice/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.boundservice
 
+import android.content.ComponentName
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import com.example.boundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            val binder = (p1 as? BoundService.LocalBinder) ?: return
+            val boundService = binder.getService()
+            boundService.textViewChanged = { text ->
+                binding.outputTv.text = text.toString()
+            }
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {}
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.startServiceBtn.setOnClickListener { launchService() }
+
+        bindService(BoundService.newIntent(this), serviceConnection, 0)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(serviceConnection)
+    }
+
+    private fun launchService() {
+        startService(BoundService.newIntent(this))
     }
 }
